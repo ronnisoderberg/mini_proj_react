7b7a44efa15d0e6bf60b84875966dfc3c180292a
Jakob Dennryd <jakob.dennryd@utb.ecutbildning.se>
2022-09-19 15:43:22 +0200

    imported useState

--- src/components/FormURL.jsx
+++ src/components/FormURL.jsx
@@ -1,28 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-class QRform extends React.Component {
-  render() {
-    const FormURL = () => {
-      const [url, setUrl] = useState("");
+const FormURL = () => {
+  const [url, setUrl] = useState("");
 
-      return (
-        <>
-          <div className="container">
-            <h2>Create a QR image</h2>
-            <div>Write the link you want to create as a QR image!</div>
-            <form className="url-form">
-              <label>Task</label>
-              <input
-                type="text"
-                value={url}
-                required
-                onChange={(e) => setUrl(e.target.value)}
-              />
-              <button type="button"> Create </button>
-            </form>
-          </div>
-        </>
-      );
-    };
-  }
-}
+  return (
+    <>
+      <div className="container">
+        <h2>Create a QR image</h2>
+        <div>Write the link you want to create as a QR image!</div>
+        <form className="url-form">
+          <label>Task</label>
+          <input
+            type="text"
+            value={url}
+            required
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button type="button"> Create </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default FormURL;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import FormURL from "./components/FormURL.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <FormURL />
   </React.StrictMode>
 );
